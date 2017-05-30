31bc23f35c17de55df9c715f412e4b31ce2a360c
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-05-30 17:34:12 -0500

    TST: add unit testing suite

---
+++ test/test_bulk_downloader.py
@@ -0,0 +1,35 @@
+import unittest
+from mock import patch, MagicMock
+
+from download_espa_order import Api, Scene, LocalStorage
+from mocks.espa_api import MockApiRequest
+
+
+class TestAPIInteraction(unittest.TestCase):
+    def setUp(self):
+        self.host = 'invalidhost.com'
+        self.username = 'production'
+        self.password = 'secret1'
+        self.api = Api(self.host, self.username, self.password)
+        self.email = 'production@email.com'
+
+    def tearDown(self):
+        pass
+
+    @patch('download_espa_order.Api.api_request', MockApiRequest)
+    def test_api_get_completed_scenes(self):
+        orders = self.api.retrieve_all_orders(self.email)
+        self.assertIsInstance(orders, list)
+
+
+class TestUserErrors(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+class TestAPIErrors(unittest.TestCase):
+    pass
