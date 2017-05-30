6d1068d20da4645a2d297c91cc0b1a0c29aeecd0
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-05-30 17:33:38 -0500

    DEV: add mock api requests

---
+++ mocks/espa_api.py
@@ -0,0 +1,14 @@
+class MockApiRequest(dict):
+    def __init__(self, *args, **kwargs):
+        super(MockApiRequest, self).__init__()
+        url = args[0]
+
+        self.store = dict()
+        if 'item-status' in url:
+            self.update({'orderid': {'production@email.com-0000-00-00': {
+                "product_dload_url": "http://download.com/filename.tar.gz"
+            }}})
+        if 'list-orders' in url:
+            self.update({'orders': ['production@email.com-0000-00-00']})
+        if 'order-status' in url:
+            self.update({'orderid': 'production@email.com-0000-00-00', 'status': 'complete'})
